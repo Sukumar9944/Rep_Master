99be0d6b58c3f5fafa21f742ce59688e3bdbfb91
Sukumar9944 <marsuku576@gmail.com>
2024-01-27 14:24:58 +0530

    add1

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+# Update package list
+sudo apt-get update
+
+# Install system-level dependencies
+sudo apt-get install -y libespeak1
+
+# Install Python dependencies
+pip install -r requirements.txt
