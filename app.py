e7942b7c68b90d72dc6b09a9dd21061f7f9cbfbd
Sukumar9944 <marsuku576@gmail.com>
2024-01-27 12:23:00 +0530

    'update3'

--- app.py
+++ app.py
@@ -2,19 +2,26 @@ import streamlit as st
 import pyttsx3
 
 # Setting Webpage Configurations
-st.set_page_config(page_icon="🎶",page_title="Audio Extracter", layout="wide")
+st.set_page_config(page_icon="",page_title="Audio Extracter", layout="wide")
 
-st.title('COUNTER APP')
+st.header(':red[Rep Master] ®')
+st.caption('Feel the rhythm, own the beat !')
 
-num_range = st.text_input('enter your rep count')
+num_range = st.text_input('Enter your Rep Count')
+
+speed = st.text_input('Enter the Speed')
 
 start_button = st.button('Start')
-    
-if start_button and num_range:
+
+def master(speed, num_range):    
     engine = pyttsx3.init()
-    engine.setProperty('rate', 100)
-    for i in range(int(num_range)):
-        print(i)
+    engine.setProperty('rate', speed)
+    for i in range(1, int(num_range)+1):
         engine.say(i)
         engine.runAndWait()
-        
+
+
+if start_button and num_range and speed:
+    master(int(speed), num_range)
+    st.success('RepMaster - Program Completed 🏋️‍')
+
