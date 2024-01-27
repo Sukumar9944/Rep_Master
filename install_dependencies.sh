4460154eb530699aa39b641ccd4996ac21e4a18e
Sukumar9944 <marsuku576@gmail.com>
2024-01-27 14:47:50 +0530

    update

--- install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-# Update package list
-sudo apt-get update
-
-# Install system-level dependencies
-sudo apt-get install -y libespeak1
-
-# Install Python dependencies
-pip install -r requirements.txt
---
+++ postinstall.sh
@@ -0,0 +1,5 @@
+# Update package list
+sudo apt-get update
+
+# Install system-level dependencies
+sudo apt-get install espeak
