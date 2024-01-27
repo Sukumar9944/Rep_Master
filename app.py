b51de7a8475d31c98a59a92e7eabfbf1e37c4eca
Sukumar9944 <marsuku576@gmail.com>
2024-01-27 08:32:48 +0530

    'update1'

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ st.set_page_config(page_icon="🎶",page_title="Audio Extracter", layout="wide")
 
 st.title('COUNTER APP')
 
-engine = pyttsx3.init()
-engine.setProperty('rate', 100)
-
 num_range = st.text_input('enter your rep count')
 
 start_button = st.button('Start')
     
 if start_button and num_range:
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 100)
     for i in range(int(num_range)):
         print(i)
         engine.say(i)
