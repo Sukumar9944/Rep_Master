37912dbaa0f0152ea28e63c3eb530b6cf3761de5
Sukumar9944 <marsuku576@gmail.com>
2024-01-30 18:12:10 +0530

    s

--- app1.py
+++ app1.py
@@ -1,10 +1,18 @@
-from playsound import playsound
 import streamlit as st
 
-st.title('suku')
-start = st.button('start')
+def main():
+    st.title("Audio Player App")
 
-if start:
-    for i in range(10):
-        playsound('https://www.freetamilringtones.com/jdownloads/dialogue_ringtones./kamal_haasan/vettaiyadu_villayadu_intro.mp3')
-        print('Finished')
+    audio_url = st.text_input("Enter the URL of the audio file:")
+    play_button = st.button("Start Playing")
+
+    if play_button:
+        if audio_url:
+            st.text("Playing audio...")
+            # Use JavaScript to play audio in the browser
+            st.markdown(f'<audio src="{audio_url}" controls autoplay></audio>', unsafe_allow_html=True)
+        else:
+            st.warning("Please enter a valid audio URL.")
+
+if __name__ == "__main__":
+    main()
