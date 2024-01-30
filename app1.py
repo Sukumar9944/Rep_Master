643be377900c78221340139770877bde4c4631af
Sukumar9944 <marsuku576@gmail.com>
2024-01-30 17:52:49 +0530

    sil

--- app1.py
+++ app1.py
@@ -1,13 +1,10 @@
 from playsound import playsound
-import os
 import streamlit as st
 
 st.title('suku')
 start = st.button('start')
 
-abs_path = os.path.abspath('sound.mp3')
-
 if start:
     for i in range(10):
-        playsound(abs_path)
+        playsound('https://www.freetamilringtones.com/jdownloads/dialogue_ringtones./kamal_haasan/vettaiyadu_villayadu_intro.mp3')
         print('Finished')
